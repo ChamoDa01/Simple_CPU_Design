3e958ebbb85c7ced619b6580c75218149ac774f4
niduniDK <158045473+niduniDK@users.noreply.github.com>
2024-04-17 22:26:16 +0530

    Update Instruction_Decoder.vhd

--- Design Source/Instruction_Decoder.vhd
+++ Design Source/Instruction_Decoder.vhd
@@ -39,6 +39,7 @@ entity Instruction_Decoder is
            LoadSelect1 : out STD_LOGIC;
            LoadSelect2 : out STD_LOGIC;
            LoadSelect3 : out STD_LOGIC;
+           minORmax : out STD_LOGIC;
            Logic_Func : out STD_LOGIC_VECTOR(1 downto 0);
            ImmediateValue : out STD_LOGIC_VECTOR (3 downto 0);
            RegSelect1 : out STD_LOGIC_VECTOR (2 downto 0);
@@ -55,6 +56,7 @@ begin
     LoadSelect1 <= not(InsBus(12)) and InsBus(11) and not(InsBus(10));
     LoadSelect2 <= InsBus(12) and (InsBus(10) or not(InsBus(11)));
     LoadSelect3 <= InsBus(12) and InsBus(11) and not(InsBus(10));
+    minORmax <= InsBus(0);
     Logic_Func <= InsBus(11 downto 10);
     ImmediateValue <= InsBus(3 downto 0);
     RegSelect1 <= InsBus(9 downto 7);
